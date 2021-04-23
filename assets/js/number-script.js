3fe414037b3e7f565dbee115a62df1cd096aab15
ali-rajabli <ali.recebli9@gmail.com>
2021-04-23 11:39:23 +0400

    commit

--- assets/js/number-script.js
+++ assets/js/number-script.js
@@ -3,3 +3,15 @@ $(window).on('load', function() {
         $('.loader-wrapper').fadeOut(400);
     }, 1000)
 })
+var num1 = $('#startNum').val();
+var num2 = $('#endNum').val();
+numbers = [];
+
+for (num1; num1 < num2; num1++) {
+    $('#random_number').html(`${num1}`)
+    numbers.push(num1);
+}
+
+$('#btn_random').on('click', function() {
+    console.log(numbers)
+})
