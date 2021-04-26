8cddf0eadb1ffd705ffbd0c5780f757ac32d641d
ali-rajabli <ali.recebli9@gmail.com>
2021-04-26 17:21:03 +0400

    1vs1 generator fixed 70%

--- assets/js/1_vs_1-script.js
+++ assets/js/1_vs_1-script.js
@@ -17,11 +17,24 @@ $('#vs_adder').on('click', function() {
 });
 
 
-
-
 $('#vs_clear').on('click', function() {
     $('#vs_input').val("");
     nameList = [];
-    $('h4').remove();
-    console.log('nameList');
+    $('.teams').remove();
+    $('#vs_list h4').remove();
+    console.log(nameList);
+})
+$("#btn_shuffle").on('click', function() {
+    if (nameList.length % 2 == 0) {
+        $('.teams').remove();
+        for (let i = 0; i < nameList.length / 2; i++) {
+            var index1 = Math.floor(Math.random() * nameList.length)
+            var index2 = Math.floor(Math.random() * nameList.length)
+            $('#random_vs').append(`<div class="teams"><h1>Team  ${i+1}</h1>
+        <h2>${nameList[index1]} vs ${nameList[index2]}</h2></div>`)
+        }
+    } else {
+        $('#random_vs').html(`<div class="teams"><h1>The names' amount must be even!</h1></div>`)
+    }
+
 })
